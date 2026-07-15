14685dc53ed81dd2878cd71065dd3a437fa709e2
Aditya Ghule <adityaghule2703@gmail.com>
2026-07-15 22:53:54 +0530

    15 july trunet first

--- src/config.js
+++ src/config.js
@@ -5,9 +5,9 @@ const config = {
   //  baseURL: import.meta.env.VITE_API_BASE_URL
 
                             // baseURL: 'http://localhost:5000/api'
-                          baseURL: 'http://103.175.190.244/api'
+                          // baseURL: 'http://103.175.190.244/api'
                           // baseURL: 'https://codiantsolutions.com/api/trunet/api'
-                          // baseURL : 'http://192.168.1.8:5000/api'
+                          baseURL : 'http://192.168.1.34:5000/api'
 };
 
 export default config;
